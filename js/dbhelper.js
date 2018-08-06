fe9a8ce466a437e14b27bfc4a620114ba114203b
Evuazeze Emmanuel Oghenewegba <evuazeze.emmanuel@gmail.com>
2018-08-06 03:14:57 +0100

    fix: display images on github pages

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -149,7 +149,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`https://evuazeze.github.io/mws-restaurant-stage-1/img/${restaurant.photograph}`);
   }
 
   /**
